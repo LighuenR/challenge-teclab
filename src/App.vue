c6e448de28f70410e7a8b4d408d75a3a2cd75219
LighuenRestanio <lighuen.restanio@gmail.com>
2021-11-04 12:46:40 -0300

    Vue-installed

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Formulario />
+    <Timeline />
+    <Tabla />
+  </div>
+</template>
+
+<script>
+import Formulario from "@/components/reservas/Formulario.vue";
+import Tabla from "@/components/reservas/Tabla.vue";
+import Timeline from "@/components/reservas/Timeline.vue";
+
+export default {
+  name: "App",
+  components: {
+    Formulario,
+    Tabla,
+    Timeline,
+  },
+};
+</script>
+
+<style>
+</style>
